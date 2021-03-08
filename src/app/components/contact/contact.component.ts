0bc257d7f1fdac474f26222f477dd15310322447
Fernando Ramirez <ferjaviramirez@gmail.com>
2021-03-08 14:14:44 +0100

    maqutacion termainada

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { DataDbService } from '../../services/data-db.service';
+import { MessageInterface } from '../../interfaces/message';
+import { FormControl, FormGroup } from '@angular/forms';
+
+
+
 
 @Component({
   selector: 'contactForm',
@@ -7,9 +13,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  contactForm: FormGroup;
+
+  constructor(private _dataService: DataDbService) { 
+    this.contactForm= this.createFormGroup();
+  }
 
   ngOnInit(): void {
+  }
+
+  createFormGroup() {
+    return new FormGroup({
+      email: new FormControl(''),
+      name: new FormControl(''),
+      message: new FormControl('')
+    });
+  }
+
+  onResetForm() {
+    this.contactForm.reset();
+  }
+
+  onSaveForm() {
+    console.log('guardado')
   }
 
 }
